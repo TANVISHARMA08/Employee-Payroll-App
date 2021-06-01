83143e62c4ad426b50f5888e82ef4730677bf63c
TANVI SHARMA <82575220+TANVISHARMA08@users.noreply.github.com>
2021-06-01 20:45:02 +0530

    Update form.js
    Addig uc14

--- js/form.js
+++ js/form.js
@@ -1,26 +1,78 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function() {
-    output.textContent = salary.value;
-});
+window.addEventListener('DOMContentLoaded', (event) =>{
 
-const text = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function() {
-    const regName = /^[A-Z][a-z]{2,}$/;
-    if (regName.test(text.value))
-        textError.textContent = "";
-    else textError.textContent = "Invalid first name ";
-});
+    const salary=document.querySelector('#salary');
+    const output=document.querySelector('.salary-output');
+       output.textContent = salary.value;
+    salary.addEventListener('input', function(){
+       output.textContent = salary.value;
+    });
 
-document.getElementById("submit").onclick = function() {
-    let employee = new EmployeePayroll();
-    employee.name = document.getElementById("name").value;
-    employee.picture = document.querySelector('input[name = profile]:checked').value;
-    employee.gender = document.querySelector('input[name = gender]:checked').value;
-    employee.department = document.querySelector('input[name = department]:checked').value;
-    employee.salary = document.getElementById("salary").value;
-    employee.notes = document.getElementById("notes").value;
-    employee.startDate = new Date(parseInt(document.getElementById("year").value), parseInt(document.getElementById("month").value), parseInt(document.getElementById("day").value));
-};
+    const name=document.querySelector('#name');
+    const textError=document.querySelector('.error-text-output');
+    name.addEventListener('input', function(){
+        if (name.value.length==0){
+            textError.textContent="";
+            return;
+        }
+        try{
+           (new EmployeePayRoll()).name = name.value;
+           textError.textContent = "";
+        }catch (e){
+            textError.textContent = e;
+        }
+    });
+});  
+
+const save = () => {
+    try{
+        let employeePayrollData = createEmployeePayroll();
+        createAndupdateStorage(employeePayrollData);
+    } catch (e){
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayRoll();
+    try{
+        employeePayrollData.name = getInputValueById('#name');
+    }catch (e){
+        setTextValue ('.error-text-output',e);
+    }
+    employeePayrollData.profile = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.notes = getInputValueById('#notes');
+    let date= getInputValueById('#day')+ " "+getInputValueById('#month')+
+              " "+ getInputValueById('#year');
+    employeePayrollData.startDate= Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+function createAndupdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.selItems("EmployeePayrollList",JSON.stringify(employeePayrollList));
+}
